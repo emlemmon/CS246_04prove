ed39066d5ca4d7cb2c8873071db14721914c6f3d
RemnantKnight56 <42821827+RemnantKnight56@users.noreply.github.com>
2021-10-14 09:42:01 -0600

    Added a hello line.

--- src/lemmon/HelloFromEmily.java
+++ src/lemmon/HelloFromEmily.java
@@ -3,5 +3,6 @@ package lemmon;
 public class HelloFromEmily {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Emily.");
+        System.out.println("Hello Emily, my name is Jeremy.");
     }
 }
