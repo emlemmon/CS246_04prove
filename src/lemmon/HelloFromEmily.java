dca16aa0873d2769878f23931088db4d1e6ba912
Nat-Hunt <83727447+Nat-Hunt@users.noreply.github.com>
2021-10-15 19:02:03 -0500

    Added hello message from Nathan

--- src/lemmon/HelloFromEmily.java
+++ src/lemmon/HelloFromEmily.java
@@ -4,5 +4,6 @@ public class HelloFromEmily {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Emily.");
         System.out.println("Hello Emily, my name is Jeremy.");
+        System.out.println("Hello Emily, my name is Nathan.");
     }
 }
